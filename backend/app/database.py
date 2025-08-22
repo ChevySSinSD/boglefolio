45f0cb5051ab737923f112229570334065acd770
ChevySSinSD <ChevySSinSD@gmail.com>
2025-08-22 14:25:00 -0700

    Correct database url

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from typing import Any, Generator
 from sqlalchemy import Engine
 from sqlmodel import SQLModel, Session, create_engine
 
-DATABASE_URL = "sqlite:///./boglefolio.db"
+DATABASE_URL = "sqlite:///backend/boglefolio.db"
 engine: Engine = create_engine(url=DATABASE_URL, echo=True)
 
 def create_db_and_tables() -> None:
